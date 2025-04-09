05f9295b4003e0bb3aa9dcec92c1eab6c7f29a61
Jan <jan.b3nes@gmail.com>
2025-04-09 18:23:35 +0200

    Add TypeMoveDirection

---
+++ project/Epidemy_Evolution_Optimalizer/TypeMoveDirection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epidemy_Evolution_Optimalizer
+{
+    public enum MoveDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
