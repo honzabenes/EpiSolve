607b38fa74926cf9cb9b93f0da6e3925438eaa3f
Jan <jan.b3nes@gmail.com>
2025-04-09 17:50:34 +0200

    Add GridPosition class

---
+++ project/Epidemy_Evolution_Optimalizer/GridPosition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epidemy_Evolution_Optimalizer
+{
+    class GridPosition
+    {
+        public uint X { get; set; }
+        public uint Y { get; set; }
+
+        public GridPosition(uint x, uint y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        private bool Equals(GridPosition other)
+        {
+            return (this.X == other.X && this.Y == other.Y);
+        }
+
+        public static bool operator == (GridPosition left, GridPosition right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator != (GridPosition left, GridPosition right)
+        {
+            return !(left.Equals(right));
+        }
+
+        public override string ToString()
+        {
+            return $"(X: {X}, Y: {Y})";
+        }
+    }
+}
