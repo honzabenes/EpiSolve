f847a48a8ad063d2fa582c977bcb5a618b78f5fa
Jan <jan.b3nes@gmail.com>
2025-04-09 17:19:31 +0200

    Add Agent class

---
+++ project/Epidemy_Evolution_Optimalizer/Agent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epidemy_Evolution_Optimalizer
+{
+    class Agent
+    {
+        public SIR Status { get; set; }
+        public ushort Age { get; set; }
+
+        public Agent(SIR status, ushort age)
+        {
+            this.Status = status;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Status: {Status}, Age: {Age}";
+        }
+    }
+}
