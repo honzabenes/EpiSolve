256ec9e0bf72c859d1b7aaeb3cf013f84cbb75c3
Jan <jan.b3nes@gmail.com>
2025-04-09 18:31:42 +0200

    Add Position property to Agent class

--- project/Epidemy_Evolution_Optimalizer/Agent.cs
+++ project/Epidemy_Evolution_Optimalizer/Agent.cs
@@ -8,13 +8,15 @@ namespace Epidemy_Evolution_Optimalizer
 {
     class Agent
     {
+        public GridPosition Position { get; set; }
         public SIR Status { get; set; }
         public AgentAge Age { get; set; }
 
-        public Agent(SIR status, AgentAge age)
+        public Agent(SIR status, AgentAge age, GridPosition position)
         {
             this.Status = status;
             this.Age = age;
+            Position = position;
         }
 
         public override string ToString()
