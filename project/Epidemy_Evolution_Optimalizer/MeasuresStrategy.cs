6fa589acf63506cd31ca29e62f61ad336ba561dc
Jan Beneš <jan.benes@mailportal.cz>
2025-04-09 20:51:51 +0200

    Add MeasuresStrategy class

---
+++ project/Epidemy_Evolution_Optimalizer/MeasuresStrategy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epidemy_Evolution_Optimalizer
+{
+    internal class MeasuresStrategy
+    {
+        double LockdownStartThreshold { get; set; }
+        double LockdownEndThreshold { get; set; }
+        double LockdownReductionFactor { get; set; }
+
+        public MeasuresStrategy(Random random)
+        {
+            this.LockdownReductionFactor = random.NextDouble();
+            this.LockdownStartThreshold = random.NextDouble();
+            do
+            {
+                this.LockdownEndThreshold = random.NextDouble();
+            } while (this.LockdownStartThreshold <= this.LockdownEndThreshold);
+        }
+
+        public MeasuresStrategy(double lockdownStartThreshold, double lockdownEndThreshold, double lockdownReductionFactor)
+        {
+            this.LockdownStartThreshold = lockdownStartThreshold;
+            this.LockdownEndThreshold = lockdownEndThreshold;
+            this.LockdownReductionFactor = lockdownReductionFactor;
+        }
+
+        public MeasuresStrategy Clone()
+        {
+            return new MeasuresStrategy(this.LockdownStartThreshold, this.LockdownEndThreshold, this.LockdownReductionFactor);
+        }
+    }
+}
