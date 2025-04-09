a576b9d9fc2a2c80372e45251e5985b315f8ce9b
Jan <jan.b3nes@gmail.com>
2025-04-09 17:06:15 +0200

    Add TypeSIR

---
+++ project/Epidemy_Evolution_Optimalizer/TypeSIR.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epidemy_Evolution_Optimalizer
+{
+    public enum SIR
+    {
+        Susceptible,
+        Infected,
+        Recovered
+    }
+}
