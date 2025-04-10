af80a839b1bac85218b67d7d66541663d7d7319b
Jan Beneš <jan.benes@mailportal.cz>
2025-04-10 23:30:52 +0200

    Add Simulate function to Simulation class

--- project/Epidemy_Evolution_Optimalizer/Simulation.cs
+++ project/Epidemy_Evolution_Optimalizer/Simulation.cs
@@ -6,39 +6,85 @@ using System.Threading.Tasks;
 
 namespace Epidemy_Evolution_Optimalizer
 {
-    class Simulation
+    static class Simulation
     {
-        public GridMap Grid { get; set; }
-        public int AgentsCount { get; set; }
-
-
-        public Simulation(GridMap grid, int agentsCount)
+        private static Agent[] InitAgents(GridMap grid, int agentsCount)
         {
-            this.Grid = grid;
-            this.AgentsCount = agentsCount;
-        }
-
-        private Agent[] InitAgents()
-        {
-            Agent[] agents = new Agent[AgentsCount];
+            Agent[] agents = new Agent[agentsCount];
             Random random = new Random();
 
-            for (int i = 0; i < AgentsCount; i++)
+            for (int i = 0; i < agentsCount; i++)
             {
-                int randomX = random.Next(Grid.Width);
-                int randomY = random.Next(Grid.Height);
+                int randomX = random.Next(grid.Width);
+                int randomY = random.Next(grid.Height);
                 GridPosition position = new GridPosition(randomX, randomY);
 
                 AgentAge[] ages = Enum.GetValues(typeof(AgentAge)).Cast<AgentAge>().ToArray();
                 int randomAgeIndex = random.Next(ages.Length);
                 AgentAge age = ages[randomAgeIndex];
 
-                SIR status = SIR.Susceptible;
+                bool isSomeoneInfected = false;
+                SIR status;
+                if (!isSomeoneInfected)
+                {
+                    status = SIR.Infected;
+                    isSomeoneInfected = true;
+                }
+                else
+                {
+                    status = SIR.Susceptible;
+                }
 
                 agents[i] = new Agent(status, age, position);
             }
 
             return agents;
+        }
+
+        private static void SetGridTileStatus(GridMap grid, Agent agent)
+        {
+            int x = agent.Position.X;
+            int y = agent.Position.Y;
+
+            if (agent.Status == SIR.Infected)
+            {
+                grid.Tiles[x, y] = TileState.HighRisk;
+            }
+        }
+
+        private static void CloseGridTileStatus(GridMap grid, Agent agent)
+        {
+            int x = agent.Position.X;
+            int y = agent.Position.Y;
+
+            grid.Tiles[x, y] = TileState.Safe;
+        }
+
+        public static int Simulate(GridMap grid, int agentsCount, int simulationTime, double infectionTransmissionProbabiltiy)
+        {
+            Random random = new Random();
+            int maxInfected = 0;
+            Agent[] agents = InitAgents(grid, agentsCount);
+            
+            for (int i = 0; i < simulationTime; i++)
+            {
+                foreach (Agent agent in agents)
+                {
+                    CloseGridTileStatus(grid, agent);
+                    agent.Move(grid, random);
+                    SetGridTileStatus(grid, agent);
+                }
+                foreach (Agent agent in agents)
+                {
+                    bool isInfected = agent.TryInfect(grid, infectionTransmissionProbabiltiy, random);
+                    if (isInfected)
+                    {
+                        maxInfected++;
+                    }
+                }
+            }
+            
+            return maxInfected;
         }
     }
 }
