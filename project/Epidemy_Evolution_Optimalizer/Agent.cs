7a238384b732f487726e5391f1a89b5c3a872c25
Jan <jan.b3nes@gmail.com>
2025-04-20 17:08:09 +0200

    Initial commit on new branch

--- project/Epidemy_Evolution_Optimalizer/Agent.cs
+++ project/Epidemy_Evolution_Optimalizer/Agent.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Epidemy_Evolution_Optimalizer
 {
